fea81bc41f2b74fdf9ed64a2ddb57ee2d0828d94
Aafreen-Afi <137905884+Aafreen-Afi@users.noreply.github.com>
2023-07-04 19:30:14 +0530

    Update ABCpattern.js

--- Apattern.js
+++ Apattern.js
@@ -1,29 +1,107 @@
+//A Pattern
 function APattern(){
+    var string_toPrint="";
     for(var i=1;i<=9; i++){
        if(i === 1){
         for(var k=1;k<=1;k++){
-            var string_toPrint=" ";
+             string_toPrint+=" ";
         }
         for(var j=1;j<=9;j++){
-            var string_toPrint="*";
+             string_toPrint+="*";
         }
-        var string_toPrint="\n";
+         string_toPrint+="\n";
        }
        else if(i===5){
         for(var j=1;j<=11;j++){
-            var string_toPrint="*";
+             string_toPrint+="*";
         }
-        var string_toPrint="\n";
+         string_toPrint+="\n";
        }
        else{
-        var string_toPrint="*";
+         string_toPrint+="*";
         for(var k=1;k<=9;k++){
-            var string_toPrint=" ";
+             string_toPrint+=" ";
         }
-        var string_toPrint="*";
-        var string_toPrint="\n";
+         string_toPrint+="*";
+         string_toPrint+="\n";
        }
-       console.log(string_toPrint)
     }
+    console.log(string_toPrint)
+}
+
+//B Pattern
+function Bpattern(){
+    var string_toPrint="";
+    for(var i=1;i<=9; i++){
+     if(i === 1){
+      for(var k=1;k<=1;k++){
+        //    string_toPrint+=" ";
+      }
+      for(var j=1;j<=10;j++){
+           string_toPrint+="*";
+      }
+       string_toPrint+="\n";
+     }
+     else if(i===5){
+      for(var j=1;j<=10;j++){
+           string_toPrint+="*";
+      }
+       string_toPrint+="\n";
+     }
+     else if(i===9){
+        for(var k=1;k<=1;k++){
+          //    string_toPrint+=" ";
+        }
+        for(var j=1;j<=10;j++){
+             string_toPrint+="*";
+        }
+         string_toPrint+="\n";
+       }
+     else{
+       string_toPrint+="*";
+      for(var k=1;k<=9;k++){
+           string_toPrint+=" ";
+      }
+       string_toPrint+="*";
+       string_toPrint+="\n";
+     }
+  }
+  console.log(string_toPrint)
+}
+
+//C Pattern
+function Cpattern(){
+    var string_toPrint="";
+    for(var i=1;i<=9; i++){
+     if(i === 1){
+      for(var k=1;k<=1;k++){
+           string_toPrint+=" ";
+      }
+      for(var j=1;j<=9;j++){
+           string_toPrint+="*";
+      }
+       string_toPrint+="\n";
+     }
+     else if(i===9){
+          for(var k=1;k<=1;k++){
+              string_toPrint+=" ";
+          }
+          for(var j=1;j<=9;j++){
+               string_toPrint+="*";
+          }
+           string_toPrint+="\n";
+         }
+     else{
+       string_toPrint+="*";
+      for(var k=1;k<=9;k++){
+           string_toPrint+=" ";
+      }
+     
+       string_toPrint+="\n";
+     }
+  }
+  console.log(string_toPrint)
 }
 APattern();
+Bpattern();
+Cpattern();
